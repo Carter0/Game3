4235ccce09a5581ce06ba24c7c684d9afa6feadd
carter <weinbergcarter@gmail.com>
2022-10-20 20:38:42 -0400

    hopefully I am starting to figure this out

--- src/main.rs
+++ src/main.rs
@@ -33,22 +33,33 @@ fn spawn_camera(mut commands: Commands) {
 #[derive(Component)]
 struct Collidable;
 
-// Alrighty a few things with this, cause I now realize you are implementing general collision,
-// which might be a whole long process by itself. I'm honestly sorta down for that, but this just got more
-// complicated, again lol.
+#[derive(Component)]
+struct Movement {
+    velocity: Vec2,
+}
 
-// Firstly, all the walls are colliding with themselves off the bat. And the way you have the collision
-// logic set up is making it so the bottom and top walls are moving to some random spot.
-// Your problem is that you don't know which transform is a and which one is b.
-// You need to know that in order for your logic to make any sense haha
+// TODO make something like this...
+// enum CollisionType {
+//     Reflect
+//     Stop
+//     Destroy)
+// }
 
-// I think you need to rethink what happens when two entities collide.
+// TODO make something like this...
+// enum RigidBodyType {
+//     Static
+//     Dynamic
+// }
+
+// Note this needs to happen AFTER movement so that it can reset the position on collision
+// create_system_set(fixedtimestep, add_system(movement).label(movement)).add_system(collisions).after(movement)
+
+// TODO query on rigidbody type
 fn check_collisions(mut collisions_query: Query<(&mut Transform, &Sprite), With<Collidable>>) {
+    // The combination is an arrangement of entities's components without repeats
     let mut combinations = collisions_query.iter_combinations_mut();
 
-    while let Some([(mut a_transform, a_sprite), (b_transform, b_sprite)]) =
-        combinations.fetch_next()
-    {
+    while let Some([(a_transform, a_sprite), (b_transform, b_sprite)]) = combinations.fetch_next() {
         let a_size: Vec2 = a_sprite
             .custom_size
             .expect("All sprites need to have custom sizes.");
@@ -57,87 +68,23 @@ fn check_collisions(mut collisions_query: Query<(&mut Transform, &Sprite), With<
             .custom_size
             .expect("All sprites need to have custom sizes.");
 
-        if let Some(collision) = collide(
+        // I think the reason you are getting tripped up here is because you have different types of rigid bodies that can have collisions
+        // Like the walls are static objects that don't move whereas the player is a dynamic object that can.
+        // NOTE I am not sure I have the terminology right here, is it static/kinematic???
+        // I think if two movement/dynamic object collide then they should reflect
+        // OR should two dynamic objects not collide at all?
+
+        // TODO create an enum for the type of objects colliding
+        if let Some(_collision) = collide(
             a_transform.translation,
             a_size,
             b_transform.translation,
             b_size,
         ) {
-            // TODO figure out what you want to happen here
-            match collision {
-                Collision::Left => {}
-                Collision::Right => {}
-                Collision::Top => {}
-                Collision::Bottom => {}
-                Collision::Inside => {}
-            }
+            // TODO have an enum here later for the collision type
         }
     }
 }
 
-// TODO player wall collisions into a general collision component
-
-// Perhaps this is not quite the right component.
-// Maybe the move is to use this more like a empty tag component
-// #[derive(Component)]
-// struct CollisionBetween {
-//     a_transform: Transform,
-//     a_size: Vec2,
-//     b_transform: Transform,
-//     b_size: Vec2,
-// }
-
-// Then down here, perhaps a good idea is to have something like
-// Query<(&mut Transform, &Sprite), With<Collidable>>)
-// You could do this but you would need to do that twice?
-// Like for the other object you want to collide with
-// But then you would need to make sure that you don't hit yourself again
-// Like you don't want a to collide with a
-// fn check_collisions(mut collisions_query: Query<&CollisionBetween>) {
-//     for CollisionBetween {
-//         mut a_transform,
-//         a_size,
-//         b_transform,
-//         b_size,
-//     } in &mut collisions_query
-//     {
-//         if let Some(collision) = collide(
-//             a_transform.translation,
-//             *a_size,
-//             b_transform.translation,
-//             *b_size,
-//         ) {
-//             match collision {
-//                 Collision::Left => {
-//                     a_transform.translation = Vec3::new(
-//                         b_transform.translation.x + b_size.x / 2.0,
-//                         a_transform.translation.y,
-//                         0.0,
-//                     );
-//                 }
-//                 Collision::Right => {
-//                     a_transform.translation = Vec3::new(
-//                         b_transform.translation.x - b_size.x / 2.0,
-//                         a_transform.translation.y,
-//                         0.0,
-//                     );
-//                 }
-//                 Collision::Top => {
-//                     a_transform.translation = Vec3::new(
-//                         a_transform.translation.x,
-//                         b_transform.translation.y + b_size.y / 2.0,
-//                         0.0,
-//                     );
-//                 }
-//                 Collision::Bottom => {
-//                     a_transform.translation = Vec3::new(
-//                         a_transform.translation.x,
-//                         b_transform.translation.y - b_size.y / 2.0,
-//                         0.0,
-//                     );
-//                 }
-//                 Collision::Inside => {}
-//             }
-//         }
-//     }
-// }
+// TODO make a movement system
+// Does fairly basic linear movement
